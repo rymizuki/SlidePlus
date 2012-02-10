2032d5a9de9338716868fcf57e95edc33806b5ae
mizuki_r <ry.mizuki@gmail.com>
2012-02-10 12:09:43 +0900

    add DB

--- app.pl
+++ app.pl
@@ -22,6 +22,52 @@ sub run {
 
 1;
 
+package DB;
+use strict;
+use warnings;
+use 5.010_000;
+use parent qw/DBIx::Sunny::Schema/;
+
+use Scope::Container;
+use Scope::Container::DBI;
+our $Scope::Container::DBI::DBI_CLASS = 'DBIx::Sunny';
+
+=pod
+
+=head1 CatLifeKossy::DB
+
+DB周りのメソッド群
+
+=head2 get_db
+
+DBのインスタンス取得メソッド
+
+    my $db = CatLifeKossy::DB->get_db;
+
+=cut
+sub get_db {
+    my $class = shift;
+
+    unless (my $instance = scope_container('db')) {
+        my $dbh = $class->_get_dbh;
+        $class->new(dbh => $dbh, readonly => 0);
+    }
+}
+
+sub _get_dbh {
+    my $class = shift;
+
+    my $dbi_info = $class->_get_dbi_info;
+    my $dbh = Scope::Container::DBI->connect(@$dbi_info);
+
+    return $dbh;
+}
+
+sub _get_dbi_info {scope_container('config')->{dbi_info}}
+
+
+1;
+
 package main;
 use strict;
 use warnings;
@@ -29,6 +75,8 @@ use 5.010_000;
 
 use Mojolicious::Lite;
 plugin 'xslate_renderer';
+
+Bootstrap->run;
 
 get '/' => sub {
     my $self  = shift;
