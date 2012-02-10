836fc7033c8fbf9c6f002bbe9d2d32c5c514aded
mizuki_r <ry.mizuki@gmail.com>
2012-02-10 20:01:20 +0900

    いろいろ微調整

--- app.pl
+++ app.pl
@@ -5,14 +5,15 @@ use 5.010_000;
 use Config::Pit;
 use Scope::Container;
 
+my $user = Config::Pit::pit_get(dev_slide_plus => require => {
+    name    => 'mysql user name',
+    pass    => 'mysql user pass',
+});
+
 sub run {
     my $sc = start_scope_container;
 
     unless (scope_container('config')) {
-        my $user = Config::Pit::pit_get(dev_slide_plus => require {
-            name    => 'mysql user name',
-            pass    => 'mysql user pass',
-        });
 
         scope_container(config => {
             dbi_info    => ['dbi:mysql:slide-plus', $user->{name}, $user->{pass}],
@@ -30,7 +31,7 @@ use parent qw/DBIx::Sunny::Schema/;
 
 use Scope::Container;
 use Scope::Container::DBI;
-our $Scope::Container::DBI::DBI_CLASS = 'DBIx::Sunny';
+local $Scope::Container::DBI::DBI_CLASS = 'DBIx::Sunny';
 
 =pod
 
@@ -57,14 +58,11 @@ sub get_db {
 sub _get_dbh {
     my $class = shift;
 
-    my $dbi_info = $class->_get_dbi_info;
+    my $dbi_info = scope_container('config')->{dbi_info};
     my $dbh = Scope::Container::DBI->connect(@$dbi_info);
 
     return $dbh;
 }
-
-sub _get_dbi_info {scope_container('config')->{dbi_info}}
-
 
 1;
 
@@ -78,18 +76,104 @@ plugin 'xslate_renderer';
 
 Bootstrap->run;
 
+
+=pod
+
+=head2 /:any_method
+
+認証の無いページ
+
+=cut
 get '/' => sub {
     my $self  = shift;
-    $self->render(
-        handler     => 'tx',
-        template    => 'index',
-        msg         => 'hallo'
-    );
-} => 'index';
 
-get '/json' => sub {
-    my $self  = shift;
-    $self->render(json => {msg => 'hello world!'});
+    my $values = {msg => 'Hello!'};
+    $self->render(index => %{$values});
 };
 
+get '/auth' => sub {
+    my $self = shift;
+
+    $self->render('auth');
+};
+
+get '/authorize' => sub {
+    my $self = shift;
+
+    $self->redirect_to('/');
+};
+
+get '/slide/show/:guid' => sub {
+    my $self = shift;
+
+    $self->render(text => 'show!'.$self->param('guid'));
+};
+
+
+=pod
+
+=head2 /:any_method
+
+認証が必要なページ
+
+=cut
+under '/' => sub {
+    my $self = shift;
+
+    if (!$self->session('is_login')) {
+        return $self->redirect_to('/authorize');
+    }
+};
+
+get '/user/register' => sub {
+    my $self = shift;
+
+    $self->render('user/register');
+};
+
+post '/user/register' => sub {
+    my $self = shift;
+};
+
+get '/user/logout' => sub {
+    my $self = shift;
+
+    $self->session(expire => 1);
+    $self->redirect_to('/');
+};
+
+
+=pod
+
+=head2 /slide/:any_method
+
+スライド管理系のページ
+
+=cut
+get '/slide/add' => sub {
+    my $self = shift;
+
+    $self->render_text('add!')
+};
+
+post '/slide/add' => sub {
+    my $self = shift;
+};
+
+get '/slide/edit/:guid' => sub {
+    my $self = shift;
+
+    $self->render_text($self->param('guid'));
+};
+
+post '/slide/edit:guid' => sub {
+    my $self = shift;
+};
+
+post '/slide/remove:guid' => sub {
+    my $self = shift;
+};
+
+
+app->log->level('debug');
 app->start;
